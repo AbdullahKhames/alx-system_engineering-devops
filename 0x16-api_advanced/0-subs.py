10164df63f5fb9df5260c89ed61662fbb9724822
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-11-08 16:23:32 +0200

    removed security breaches

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ module to test reddit api"""
-from requests.auth import HTTPBasicAuth
-from requests import get, post
+from requests import get
 
 
 def remove_zeroes(data):
@@ -60,22 +59,6 @@ Dq9DemXs-aIjVE_g613LCbuN3TnKR8nxID9EgB1GjZqOA'
     if len(ls) > 0:
         return ls[0]
     return 0
-
-
-def get_token():
-    """function to get the token for the script using my credentials"""
-    client_auth = HTTPBasicAuth('RYaeF3qkHq6oFapgI7pb3Q',
-                                'DKGtb-pj9jH_j-1_eNZSK752-io_tw')
-    post_data = {"grant_type": "password",
-                 "username": "5miiss96",
-                 "password": "a37831654"}
-    headers = {
-        'User-Agent': 'script:test.api:v1 (by /u/5miiss96)'
-    }
-    response = post("https://www.reddit.com/api/v1/access_token",
-                    auth=client_auth, data=post_data,
-                    headers=headers)
-    return response.json().get('access_token')
 
 
 if __name__ == '__main__':
