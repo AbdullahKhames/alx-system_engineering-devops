f483ba731698eab794325c359009790853b21047
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-11-09 16:11:16 +0200

    fix 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -34,22 +34,10 @@ def number_of_subscribers(subreddit):
     """function to get the number of subscribers for a given subreddit
     @subreddit: the subreddit to search for
     """
-    full_url = 'https://oauth.reddit.com/r/' + subreddit + ''
+    full_url = 'https://www.reddit.com/r/' + subreddit + '.json'
 
     headers = {
         'User-Agent': 'script:test.api:v1 (by /u/5miiss96)',
-        'Authorization': 'bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI\
-1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdW\
-NZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjk5NTI5NTE2\
-LjExNTc5NywiaWF0IjoxNjk5NDQzMTE2LjExNTc5NywianRpIjoiMWRYMmZKSXp0\
-V3BBQjJCdHpGY21xMUoxZVc5aUZnIiwiY2lkIjoiUllhZUYzcWtIcTZvRmFwZ0k3cGI\
-zUSIsImxpZCI6InQyX21qNGJoNjdncyIsImFpZCI6InQyX21qNGJoNjdncyIsImxjYSI\
-6MTY5ODI2NzU4Mzc5OSwic2NwIjoiZUp5S1Z0SlNpZ1VFQUFEX193TnpBU2MiLCJmbG8i\
-Ojl9.CZP6Do2857JfHiYvt0giq8yTbym3tkPN3qC1ClYrpzKCYb6pNDdT1L45EaM8c17zWSg\
--MNnyAyaZkRDE6ddb6tnL6pvu6EHHppfrYqdSK9v4M7edxcueWb_taYlpKFDO3CMbMtGbJTtPX\
-5C3tjgklw3RbPd24K65GoPNLdvxX6KFB1cRb6nGS4sy0N_ooIfr9d909xtpNA5OjDZtksr3qpYrNp\
-VRiEKeJ_H95Oxl_FNHuFFdSxNo30pvvLxvh2SEhhqoofg_C5mzsKo3-VZrYWzTk1zRFa8RGxrlJs0Wx\
-Dq9DemXs-aIjVE_g613LCbuN3TnKR8nxID9EgB1GjZqOA'
     }
     response = get(full_url, headers=headers)
     if response.status_code != 200:
@@ -62,4 +50,4 @@ Dq9DemXs-aIjVE_g613LCbuN3TnKR8nxID9EgB1GjZqOA'
 
 
 if __name__ == '__main__':
-    pass
+    number_of_subscribers('programming')
